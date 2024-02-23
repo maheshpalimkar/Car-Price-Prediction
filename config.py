b400c2fd663f96e7090c431af17df65fc0524fb2
maheshpalimkar <maheshpalimkar825@gmail.com>
2024-02-23 16:52:14 +0530

    Added configuration File.

---
+++ config.py
@@ -0,0 +1,6 @@
+label_file_path = r'artifacts\label_encoder.pkl'
+onehot_file_path = r'artifacts\onehot_encoder.pkl'
+pca_file_path = r'artifacts\pca_model.pkl'
+model_file_path = r'artifacts\Car_price_mode.pkl'
+
+PORT_NUMBER = 8080
