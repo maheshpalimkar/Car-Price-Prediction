b355fdadbcf0cffe4d7cf3bab0a060e63ce3fbd1
maheshpalimkar <maheshpalimkar825@gmail.com>
2024-03-05 17:47:33 +0530

    updated all required files.

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ onehot_file_path = r'artifacts\onehot_encoder.pkl'
 pca_file_path = r'artifacts\pca_model.pkl'
 model_file_path = r'artifacts\Car_price_mode.pkl'
 
-PORT_NUMBER = 8080
+PORT_NUMBER = 8081
