fc5b832bc442cf373901714dbafd2907b835827e
maheshpalimkar <maheshpalimkar825@gmail.com>
2024-03-05 19:24:21 +0530

    resolved the corrections.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-label_file_path = r'artifacts\label_encoder.pkl'
-onehot_file_path = r'artifacts\onehot_encoder.pkl'
-pca_file_path = r'artifacts\pca_model.pkl'
-model_file_path = r'artifacts\Car_price_mode.pkl'
+LABEL_FILE_PATH = r'artifacts\label_encoder.pkl'
+ONEHOT_FILE_PATH = r'artifacts\onehot_encoder.pkl'
+PCA_FILE_PATH = r'artifacts\pca_model.pkl'
+MODEL_FILE_PATH = r'artifacts\Car_price_mode.pkl'
 
 PORT_NUMBER = 8081
